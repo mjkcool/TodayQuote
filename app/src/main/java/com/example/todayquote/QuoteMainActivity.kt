13fe091d448a2c7b7dbb390e8fb532a946c8fe2a
minjeong <cpflqhdlrmsu@gmail.com>
2020-11-03 06:54:35 +0900

    Desktop / 10.22 finish

--- app/src/main/java/com/example/todayquote/QuoteMainActivity.kt
+++ app/src/main/java/com/example/todayquote/QuoteMainActivity.kt
@@ -11,17 +11,21 @@ class QuoteMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.quote_main_activity)
 
+        //[정리] 세미콜론 필요없음
         var quoteText = findViewById<TextView>(R.id.Quote)
         var quoteFrom = findViewById<TextView>(R.id.Person)
 
+        //[정리] 코틀린은 인스턴스 생성시 new 식별자가 필요없음.
         var quotes = mutableListOf<Quote>()
         quotes.add(Quote(1, "명언 내용 1", "출처 1"))
         quotes.add(Quote(2, "명언 내용 2", "출처 2"))
         quotes.add(Quote(3, "명언 내용 3", "출처 3"))
 
-        var randomIndex = Random.nextInt(0, 3)
-        var randomQuote = quotes[randomIndex]
+        var randomIndex = Random.nextInt(0, 3) //[정리] 0~2
+        var randomQuote = quotes.get(randomIndex)//quotes[randomIndex]
 
+        //[정리] getter, setter가 정의되어있으면 속성 이름만으로 접근 가능
+        //text 속성값에 접근(setText())
         quoteText.text = randomQuote.sentence
         quoteFrom.text = randomQuote.from
     }
