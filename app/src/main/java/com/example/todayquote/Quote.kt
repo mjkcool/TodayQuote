57af940fed7243a196ddf4d9233231e682839c81
minjeong <cpflqhdlrmsu@gmail.com>
2020-11-14 18:31:20 +0900

    Implement almost MainActivity / Desktop / on 10.26

--- app/src/main/java/com/example/todayquote/Quote.kt
+++ app/src/main/java/com/example/todayquote/Quote.kt
@@ -1,12 +1,48 @@
 package com.example.todayquote
 
+import android.content.SharedPreferences
+
 /* [정리]
 코틀린의 '데이터 클래스'.
 - 데이터(정적 속성)을 위주로 하는 클래스: 메서드 잘 사용X
 ex) data class Person(var name:String, var age:Int)
 */
-data class Quote(
-    var idx:Int,
-    var sentence:String,
-    val from:String
-)
+data class Quote(var idx:Int, var text:String, val from:String){
+    companion object { //클래스 메서드
+
+        fun saveToPreference(
+            pref: SharedPreferences,
+            idx: Int, text: String, from: String = ""
+        ): Quote {
+            //SharedPreference 객체에 명언 데이터 저장
+            var editor = pref.edit()
+            //키, 값 저장
+            editor.putString("$idx.text", text) //명언 문장
+            editor.putString("$idx.from", from)
+            editor.apply()
+
+            return Quote(idx, text, from)
+        }
+
+        fun removeQuoteFromPreference(pref: SharedPreferences, idx: Int) {
+            //해당 위치의 데이터 제거
+            var editor = pref.edit()
+            editor.remove("$idx.text")
+            editor.remove("$idx.from")
+            editor.apply()
+        }
+
+        fun getQuotesFromPreference(pref: SharedPreferences) : List<Quote>{
+            val quotes = mutableListOf<Quote>()
+
+            for (i in 0 until 20) { //i가 0부터 19까지
+                val quoteText = pref.getString("$i.text", "")!! //null 불허타입으로 바꿈
+                val quoteFrom = pref.getString("$i.from", "")!!
+                if(quoteText.isNotBlank()){
+                    quotes.add(Quote(i, quoteText, quoteFrom))
+                }
+            }
+            return quotes
+        }
+    }
+}
--- app/src/main/java/com/example/todayquote/QuoteMainActivity.kt
+++ app/src/main/java/com/example/todayquote/QuoteMainActivity.kt
@@ -1,13 +1,35 @@
 package com.example.todayquote
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import kotlin.random.Random
 
 class QuoteMainActivity : AppCompatActivity() {
+    //Activity는 생성자는 안드로이드만 접근할 수 있음.
+    /* lateinit: 생성자에서 초기화를 해주지 못하나 null을 허용하지 않도록 한다.
+                 반드시 이 객체에 접근하기 전 이 객체를 초기화하겠다는 선언이자 약속
+     */
+    private lateinit var pref: SharedPreferences //생성만 하였을 때 null 허용 X
+    private lateinit var quotes: List<Quote> //때문에 사용전 반드시 초기화 필요
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    fun initializeQuotes(){
+        val initialized = pref.getBoolean("initialized", false)
+        if(!initialized){ //초기화 여부 확인
+
+            Quote.saveToPreference(pref, 0, "명언", "사람")
+
+            val editor = pref.edit()
+            editor.putBoolean("initialized", true)
+            editor.apply()
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) { //생성 시점에 호출되는 콜백 함수
         super.onCreate(savedInstanceState)
         setContentView(R.layout.quote_main_activity)
 
@@ -15,18 +37,54 @@ class QuoteMainActivity : AppCompatActivity() {
         var quoteText = findViewById<TextView>(R.id.Quote)
         var quoteFrom = findViewById<TextView>(R.id.Person)
 
+
+        //Preference 객체 가져오기 (파일 이름은 quotes)
+        pref = this.getSharedPreferences("quotes", Context.MODE_PRIVATE)
+        initializeQuotes()
+
+        quotes = Quote.getQuotesFromPreference(pref)
+        if(quotes.isNotEmpty()){
+            val randomIdx = Random.nextInt(quotes.size)
+            val randomQuote = quotes[randomIdx]
+            quoteText.text = randomQuote.text
+            quoteFrom.text = randomQuote.from
+        }else{
+            quoteText.text = "저장된 명언이 없습니다."
+            quoteFrom.text = ""
+        }
+
         //[정리] 코틀린은 인스턴스 생성시 new 식별자가 필요없음.
-        var quotes = mutableListOf<Quote>()
-        quotes.add(Quote(1, "명언 내용 1", "출처 1"))
-        quotes.add(Quote(2, "명언 내용 2", "출처 2"))
-        quotes.add(Quote(3, "명언 내용 3", "출처 3"))
+//        var quotes = mutableListOf<Quote>()
+//        quotes.add(Quote(1, "명언 내용 1", "출처 1"))
+//        quotes.add(Quote(2, "명언 내용 2", "출처 2"))
+//        quotes.add(Quote(3, "명언 내용 3", "출처 3"))
+//
+//        var randomIndex = Random.nextInt(0, 3) //[정리] 0~2
+//        var randomQuote = quotes.get(randomIndex)//quotes[randomIndex]
+//
+//        //[정리] getter, setter가 정의되어있으면 속성 이름만으로 접근 가능
+//        //text 속성값에 접근(setText())
+//        quoteText.text = randomQuote.sentence
+//        quoteFrom.text = randomQuote.from
 
-        var randomIndex = Random.nextInt(0, 3) //[정리] 0~2
-        var randomQuote = quotes.get(randomIndex)//quotes[randomIndex]
+        /*
+        var sp = getSharedPreferences("filename", Context.MODE_PRIVATE);
 
-        //[정리] getter, setter가 정의되어있으면 속성 이름만으로 접근 가능
-        //text 속성값에 접근(setText())
-        quoteText.text = randomQuote.sentence
-        quoteFrom.text = randomQuote.from
+        //데이터를 저장
+        val editor = sp.edit();
+
+        //키, 값 쌍이 필요(자바의 맵, 파이썬의 딕셔너리), 덮어쓰기 역시 가능
+        editor.putInt("k1", 1)
+        editor.putString("k2", "hello")
+        
+        editor.apply() // 데이터 저장시키기. = editor.commit()
+        
+        데이터 삭제
+        editor.remove("k1")
+        editor.clear()
+
+        var value1 = sp.getInt("k1", -1) //default값을 지정해야함
+        var value2 = sp.getString("k2", null)
+        */
     }
 }
